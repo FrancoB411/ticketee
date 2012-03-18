1d44636e8410f8d96e5cb594d071d0531e307818
Linda <lin.m.gonzalez@gmail.com>
2012-03-17 21:28:51 -0400

    initial commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,29 @@
+class ProjectsController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(params[:project])
+    @project.save
+    
+    redirect_to( @project, {:notice => "Project has been created."})
+  end
+  
+  def show
+    @project = Project.find(params[:id])
+  end
+  
+  # def edit
+  #   @project = Project.find(params[:id])
+  # end
+  # 
+  # def update
+  #   @project = Project.new(params[:project])
+  #   @project.update(params[:id])
+  # end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '18f34a9d8afeab419be58c3cc279fe59baa2aa65a64572e5a347d752140d31dc0c3b130c9b78c9712de22b02b8f72e989f41ca146ee269fd91b509c391d08ceb'
