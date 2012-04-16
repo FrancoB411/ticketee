c7e984b222ca9993d90e1fb043cb2e59aa639798
Linda <lin.m.gonzalez@gmail.com>
2012-04-16 19:57:14 -0400

    Restricting creating tickets to only users who have permissions to do it

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :test do
 end
 
 gem 'devise', '~> 1.4.3'
+gem 'cancan'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,6 +5,7 @@ class TicketsController < ApplicationController
                                         :edit,
                                         :update,
                                         :destroy]
+  before_filter :authorize_create!, :only => [:new, :create]
   
   def new
     @ticket = @project.tickets.build
@@ -60,5 +61,12 @@ private
 
   def find_ticket
     @ticket = @project.tickets.find(params[:id])
+  end
+  
+  def authorize_create!
+   if !current_user.admin? && cannot?("create tickets".to_sym, @project)
+      flash[:alert] = "You cannot create tickets on this project."
+      redirect_to @project
+    end
   end
 end
