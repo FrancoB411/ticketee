5e6909048f54d726c2481c88d251e04aebeb7a55
FrancoB411 <Franco@temedigital.com>
2012-05-21 19:46:40 -0400

    Users can add tags when adding a comment

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -16,7 +16,8 @@ describe CommentsController do
     end
   
     it "cannot transition a state by passing through state_id" do
-      post :create,  {  :comment    => {  :text       =>  "Hacked!",
+      post :create,  {  :tags       => " ",
+                        :comment    => {  :text       =>  "Hacked!",
                                           :state_id   =>  state.id },
                         :ticket_id  => ticket.id }
       ticket.reload
